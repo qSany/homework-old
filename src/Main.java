7ce775efbe020fe9a964169288fed79f11131e27
admin-rez <kraken_1111@mail.ru>
2023-10-04 15:46:00 +0300

    Старая домашка

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+public class Homework {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        var x1 = 763789;
+        var y1 = 4;
+        var result = x1 + y1;
+        System.out.println("x + y = " + result);
+        var x2 = 8.0;
+        var y2 = 4;
+        var result2 = x2 + y2;
+        System.out.println("x + y = " + result2);
+        var x3 = 3.6;
+        var y3 = 4;
+        var result3 = x3 + y3;
+        System.out.println("x + y = " + result3);
+
+        var x4 = 8.0;
+        var y4 = 3.5;
+        var result4 = x4 - y4;
+        System.out.println("x + y = " + result4);
+        var x5 = 3.6;
+        var y5 = 1.6;
+        var result5 = x5 - y5;
+        System.out.println("x + y = " + result5);
+        var x6 = 763789;
+        var y6 = 7639;
+        var result6 = x6 - y6;
+        System.out.println("x + y = " + result6);
+
+
+
+        var friend = 19;
+        System.out.println(friend);
+
+        var friend2 = 19;
+        var x7 = 2;
+        var result7 = friend2 + x7;
+        System.out.println("friend2 + x7 = " + result7);
+
+        var friend3 = 19;
+        var x8 = 7;
+        var result8 = friend3 / x8;
+        System.out.println("friend3 / x7 = " + result8);
+
+        var boks1 = 78.2;
+        var boks2 = 82.7;
+        var massa = boks1 + boks2;
+        System.out.println("Общая масса двух бойцов " + massa + " кг ");
+
+
+        var boks3 = 82.7;
+        var boks4 = 78.2;
+        var massa2 = boks3 - boks4;
+        System.out.println(" разницу между массами бойцов" + massa2 + " кг ");
+
+
+
+        var boks6 = 82.7;
+        var boks5 = 78.2;
+        var massa3 = boks6 - boks5;
+        System.out.println(" разницу масс спортсменов" + massa3 + " кг ");
+
+        var boks7 = 82.7;
+        var boks8 = 78.2;
+        var overload = boks7 % boks8;
+        System.out.println("Перегруз на " + overload + " кг!");
+
+        var sotrudnik1 = 640;
+        var sotrudnik2 = 8;
+        var sotrudnik = sotrudnik1 / sotrudnik2;
+        System.out.println(" Всего работников в компании " + sotrudnik + "человек" );
+
+
+        var sotrudnik3 = 94;
+        var sotrudnik4 = 80;
+        var sotrudnik5 = sotrudnik3 + sotrudnik4;
+        System.out.println(" Если в компании работает " + sotrudnik5 + "человек" );
+
+
+
+
+    }
+
+
+}
